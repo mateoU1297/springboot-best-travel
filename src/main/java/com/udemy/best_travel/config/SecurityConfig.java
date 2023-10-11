f98a096306f2240229530667487574356e0a57a5
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-10-10 22:06:21 -0500

    se agregan metodos para configuración de spring security

--- src/main/java/com/udemy/best_travel/config/SecurityConfig.java
+++ src/main/java/com/udemy/best_travel/config/SecurityConfig.java
@@ -1,17 +1,52 @@
 package com.udemy.best_travel.config;
 
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
 @Configuration
 public class SecurityConfig {
+
+    @Value("${app.client.id}")
+    private String clientId;
+    @Value("${app.client.secret}")
+    private String clientSecret;
+    @Value("${app.client-scope-read}")
+    private String scopeRead;
+    @Value("${app.client-scope-write}")
+    private String scopeWrite;
+    @Value("${app.client-redirect-debugger}")
+    private String redirectUri1;
+    @Value("${app.client-redirect-spring-doc}")
+    private String redirectUri2;
 
     private static final String[] PUBLIC_RESOURCES = {"/fly/**","/hotel/**","/swagger-ui/**", "/.well-known/**, ", "/v3/api-docs/**"};
     private static final String[] USER_RESOURCES = {"/tour/**","/ticket/**","/reservation/**"};
@@ -19,6 +54,13 @@ public class SecurityConfig {
     private static final String LOGIN_RESOURCE = "/login";
     private static final String ROLE_ADMIN = "ROLE_ADMIN";
 
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(@Qualifier(value = "appUserService") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
@@ -30,6 +72,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
     public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
         http.formLogin()
                 .and()
@@ -48,7 +91,66 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationProvider authenticationProvider(BCryptPasswordEncoder encoder) {
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(encoder);
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(BCryptPasswordEncoder encoder) {
+        var registeredClient = RegisteredClient
+                .withId(UUID.randomUUID().toString())
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .scope(scopeRead)
+                .scope(scopeWrite)
+                .redirectUri(redirectUri1)
+                .redirectUri(redirectUri2)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        return null;
+    }
+
+    @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    private static KeyPair generateRSA() {
+        KeyPair keyPair = null;
+        try {
+            var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch(NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+        return keyPair;
+    }
+
+    private static RSAKey generateKeys() {
+        var keyPair = generateRSA();
+        var publicKey = (RSAPublicKey) keyPair.getPublic();
+        var privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString()).build();
+    }
+
 }
